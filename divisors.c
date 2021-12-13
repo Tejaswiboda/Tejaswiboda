b9c1fe6019d494b3da12b7ba2803244a44b8aaef
Tejaswiboda <95487234+Tejaswiboda@users.noreply.github.com>
2021-12-13 14:48:01 +0530

    Add files via upload

---
+++ divisors.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int x;
+	printf("enter a value for x ");
+	scanf("%d",&x);
+	if(x%3==0)
+{
+	if(x%5==0)
+
+printf("it is divisible by 3 and 5");
+{
+	else
+	{
+		printf("it is not divisible by 3 and 5");
+	}
+}
+}
