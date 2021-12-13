29bab33621b0efd7ca64f2f066435834333ceae5
Tejaswiboda <95487234+Tejaswiboda@users.noreply.github.com>
2021-12-13 14:46:57 +0530

    Add files via upload

---
+++ fooditems.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+main()
+{
+	char k;
+	int quantity,bill;
+	printf(b-burger,f-french fries,p-pizza,s-sandwich);
+	scanf("%c",&k)
+	switch(k);
+	{
+		case b:
+			printf("asked for burger");
+			printf("enter the quantity");
+			scanf("%d",&quantity);
+			bill=quantity*200;
+			break;
+		case f:
+			printf("asked for french fries");
+			printf("enter the quantity");
+			scanf("%d",&quantity);
+			bill=quantity*50;
+			break;
+		case p:
+			printf("asked for pizza");
+			printf("enter the quantity");
+			scanf("%d"&quantity);
+			bill=quantity*150;
+			break;
+		case s:
+			printf("asked for sandwiches ");
+			printf("enter the quantity");
+			scanf("%d"&quantit);
+			bill=quantity*150;
+			break;
+		default:
+			printf("user has entered some incorrect value");
+			printf("enter total %d",total);
+		
+		
+	}
+}
