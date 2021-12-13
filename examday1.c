65c7d91f1d81fd3ad8e2669fc5aa44a7406bb22c
Tejaswiboda <95487234+Tejaswiboda@users.noreply.github.com>
2021-12-13 14:47:30 +0530

    Add files via upload

---
+++ examday1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	float s1,s2,s3;
+	scanf("%f%f%f",&s1,&s2,&s3);
+	if()
+	printf("%c is a uppercase letter",ch);
+	else
+	printf("%c is a lowercase letter",ch);.
+} 
